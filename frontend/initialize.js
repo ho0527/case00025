e5c90bc6b6ba501e65e1514fdbbde3a3f93931e2
chris960527 <98445729+ho0527@users.noreply.github.com>
2024-08-11 10:56:47 +0800

    fix: base update
    updatedate->20240811

--- frontend/initialize.js
+++ frontend/initialize.js
@@ -1,8 +1,6 @@
 const WEBLSNAME="case00025-"
 const AJAXURL=""
 
-document.querySelectorAll("base")[0].href="https://ho0527.github.io/case00025/"
-
 document.getElementById("lineservice").onpointerover=function(){
 	document.getElementById("lineimagehover").style.display="block"
 	document.getElementById("lineimagenothover").style.display="none"
